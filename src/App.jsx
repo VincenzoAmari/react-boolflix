270c3e4513c93e001fba826848720da658f1bea0
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-05 17:34:23 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { GlobalProvider } from "./context/MovieContext";
+import Header from "./components/Header";
+import Filters from "./components/Filters/SearchBar";
+import FilterSelect from "./components/Filters/FilterSelect";
+import Main from "./components/Main";
 
-function App() {}
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+        <Filters />
+        <FilterSelect />
+      </div>
+      <Main />
+    </div>
+  );
+};
 
 export default App;
