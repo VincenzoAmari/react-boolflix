4d43f8edaa838d276f0b31170193ca145a5447b5
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-05 17:28:07 +0100

    movie context

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,1 +1,30 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
+
+export const MovieContext = createContext();
+
+export const MovieProvider = ({ children }) => {
+  const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+  return (
+    <MovieContext.Provider
+      value={{
+        movies,
+        setMovies,
+        series,
+        setSeries,
+        search,
+        setSearch,
+        isLoading,
+        setIsLoading,
+        selectedGenre,
+        setSelectedGenre,
+      }}
+    >
+      {children}
+    </MovieContext.Provider>
+  );
+};
